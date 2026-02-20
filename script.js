a490951c19c38b1d66a48081b47ff5b898e35228
creatorblueprint <192785251+creatorblueprint@users.noreply.github.com>
2026-02-20 13:00:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 const chatBox = document.getElementById("chatBox");
 const inputField = document.getElementById("messageInput");
 const sendBtn = document.getElementById("sendBtn");
-const typingIndicator = document.getElementById("typingIndicator");
 
 function addMessage(text, sender) {
   const msgDiv = document.createElement("div");
   msgDiv.classList.add("message", sender);
   msgDiv.innerText = text;
   chatBox.appendChild(msgDiv);
+  setTimeout(() => {
+  chatBox.scrollTop = chatBox.scrollHeight;
+}, 50);
+}
+
+
+function showTyping() {
+  const typingDiv = document.createElement("div");
+  typingDiv.classList.add("message", "bot");
+  typingDiv.id = "typingBubble";
+  
+  typingDiv.innerHTML = `
+    <div class="typing-bubble">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  `;
+  
+  chatBox.appendChild(typingDiv);
   chatBox.scrollTop = chatBox.scrollHeight;
 }
+
+function removeTyping() {
+  const typingBubble = document.getElementById("typingBubble");
+  if (typingBubble) typingBubble.remove();
+}
+
 
 async function sendMessage() {
   const userMessage = inputField.value.trim();
@@ -18,7 +44,7 @@ async function sendMessage() {
   addMessage(userMessage, "user");
   inputField.value = "";
   
-  typingIndicator.style.display = "block";
+  showTyping();
   
   try {
     const response = await fetch("https://petal2-backend.onrender.com/chat", {
@@ -32,18 +58,22 @@ async function sendMessage() {
     
     const data = await response.json();
     
-    typingIndicator.style.display = "none";
-    
-    if (response.ok) {
-      addMessage(data.reply, "bot");
-    } else {
-      addMessage(data.error || "Error occurred", "bot");
-    }
+   removeTyping();
+
+if (response.status === 429) {
+  addMessage("I'm a little tired today 💗 I've talked a lot already. Let me rest and we'll chat again soon 🌸", "bot");
+}
+else if (response.ok) {
+  addMessage(data.reply, "bot");
+}
+else {
+  addMessage("Something went wrong. Try again later.", "bot");
+}
     
   } catch (error) {
-    typingIndicator.style.display = "none";
-    addMessage("Server error ☁️", "bot");
-  }
+  removeTyping();
+  addMessage("server error", "bot");
+}
 }
 
 sendBtn.addEventListener("click", sendMessage);
